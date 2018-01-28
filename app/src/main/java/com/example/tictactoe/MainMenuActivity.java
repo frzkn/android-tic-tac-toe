ccb8eb974079aecc69ddf173f0f0d3bf1ea60df5
Faraz <farazrk001@gmail.com>
2018-01-28 20:08:59 +0530

    Add MainMenu.

---
+++ app/src/main/java/com/example/tictactoe/MainMenuActivity.java
@@ -0,0 +1,23 @@
+package com.example.tictactoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void startGame(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+
+    }
+}
